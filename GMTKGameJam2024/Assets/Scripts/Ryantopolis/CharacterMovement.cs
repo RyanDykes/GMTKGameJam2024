0c05890d2edc80a27bdbba81e08187e983f6c3da
Ryan Dykes <40364132+RyanDykes@users.noreply.github.com>
2024-08-17 23:36:27 +0100

    Adding multi tendrils

--- GMTKGameJam2024/Assets/Scripts/Ryantopolis/CharacterMovement.cs
+++ GMTKGameJam2024/Assets/Scripts/Ryantopolis/CharacterMovement.cs
@@ -12,17 +12,17 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private bool isCarrying = true;
 
     [Header("Rayacsts")]
-    [SerializeField] private Transform groundRaycastOrigin;
+    [SerializeField] private float groundDistance = 0.2f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float raycastLength = 0.6f;
-    [SerializeField] private float groundDistance = 0.2f;
-
+    [SerializeField] private Transform bodyRaycastOrigin;
+    [SerializeField] private List<Transform> raycastOrigins = new List<Transform>();
+    
     [Header("Movement")]
     [SerializeField] private float speed = 2f;
 
     [Header("Rotation")]
     [SerializeField] private float rotationSpeed = 6f;
-    [SerializeField] private AnimationCurve additionalAngleCurve = null;
 
     private Camera mainCam = null;
     private Rigidbody rbody = null;
@@ -41,33 +41,25 @@ public class CharacterMovement : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         inputDirection = new Vector3(horizontal, 0f, vertical);
-        
-        Ray ray = new Ray(groundRaycastOrigin.position, -transform.up);
-        if (Physics.Raycast(ray, out RaycastHit hit, raycastLength, groundLayer))
+
+        if (inputDirection.magnitude > 0)
         {
-            Debug.DrawRay(hit.point, hit.normal * raycastLength);
-            MoveAlongSurface(hit.point, hit.normal);
+            Move();
+            Rotate();
+
+            //print("GetAverageDistance: " + GetAverageDistance());
+            //LimitDistanceFromSurface();
         }
-        else
-        {
+
+        //If no ground is hit then fallback to basic movement and apply gravity
+        Ray ray = new Ray(bodyRaycastOrigin.position, bodyRaycastOrigin.forward);
+        if (!Physics.Raycast(ray, out RaycastHit hit, raycastLength, groundLayer))
             FallbackMovement();
-        }
     }
 
     private void FixedUpdate()
     {
         rbody.velocity = Vector3.zero;
-    }
-
-    private void MoveAlongSurface(Vector3 surfacePoint, Vector3 surfaceNormal)
-    {
-        if (inputDirection.magnitude > 0)
-        {
-            Move();
-            Rotate(surfaceNormal);
-
-            LimitDistanceFromSurface();
-        }
     }
 
     private void Move()
@@ -76,33 +68,37 @@ public class CharacterMovement : MonoBehaviour
         transform.localPosition += moveDirection * speed * Time.deltaTime;
     }
 
-    private void Rotate(Vector3 surfaceNormal)
+    private void Rotate()
     {
-        Quaternion toGroundRotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, toGroundRotation, rotationSpeed * Time.deltaTime);
+        Vector3 surfaceNormal = GetAverageNormals();
 
+        //Convert movement direction to normal
         Quaternion surfaceRotation = Quaternion.FromToRotation(Vector3.up, surfaceNormal);
         Vector3 rotatedDirection = surfaceRotation * inputDirection;
         Vector3 moveDirection = rotatedDirection.normalized;
 
+        //Roate to normal
+        Quaternion toGroundRotation = Quaternion.FromToRotation(transform.up, surfaceNormal) * transform.rotation;
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, toGroundRotation, rotationSpeed * Time.deltaTime);
+
+        //Rotate to movement direction
         Quaternion targetRotation = Quaternion.LookRotation(moveDirection, transform.up).normalized;
-        Debug.DrawRay(groundRaycastOrigin.position, moveDirection, color: Color.blue);
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+#if UNITY_EDITOR
+        Debug.DrawRay(transform.position, moveDirection, color: Color.blue);
+#endif
     }
 
     private void LimitDistanceFromSurface()
     {
-        Ray ray = new Ray(groundRaycastOrigin.position, -transform.up);
-        if (Physics.Raycast(ray, out RaycastHit hit, raycastLength, groundLayer))
-        {
-            float currentDistance = hit.distance;
+        float currentDistance = GetAverageDistance();
 
-            // A small threshold to avoid jitter
-            if (Mathf.Abs(currentDistance - groundDistance) > 0.01f)
-            {
-                Vector3 targetPosition = transform.localPosition + transform.up * (groundDistance - currentDistance);
-                transform.localPosition = targetPosition;
-            }
+        // A small threshold to avoid jitter
+        if (Mathf.Abs(currentDistance - groundDistance) > 0.01f)
+        {
+            Vector3 targetPosition = transform.localPosition + transform.up * (groundDistance - currentDistance);
+            transform.localPosition = targetPosition;
         }
     }
 
@@ -112,10 +108,62 @@ public class CharacterMovement : MonoBehaviour
         return isCarrying ? transform.forward : -transform.forward;
     }
 
+    private Vector3 GetAverageNormals()
+    {
+        Vector3 surfaceNormal = transform.up;
+        Vector3 averageNormal = Vector3.zero;
+
+        foreach (Transform origin in raycastOrigins)
+        {
+            Ray ray = new Ray(origin.position, origin.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, raycastLength, groundLayer))
+            {
+#if UNITY_EDITOR
+                Debug.DrawRay(origin.position, origin.forward * raycastLength);
+#endif
+                averageNormal += hit.normal;
+            }
+        }
+
+        if (averageNormal.magnitude > 0)
+            surfaceNormal = averageNormal / raycastOrigins.Count;
+
+        surfaceNormal = surfaceNormal.normalized;
+        return surfaceNormal;
+    }
+
+    private float GetAverageDistance()
+    {
+        float surfaceDistance = groundDistance;
+        float averageDistance = 0f;
+
+        foreach (Transform origin in raycastOrigins)
+        {
+            Ray ray = new Ray(origin.position, origin.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, raycastLength, groundLayer))
+            {
+#if UNITY_EDITOR
+                Debug.DrawRay(origin.position, origin.forward * raycastLength);
+#endif
+                averageDistance += hit.distance;
+            }
+        }
+
+        if (averageDistance > 0)
+            surfaceDistance = averageDistance / raycastOrigins.Count;
+
+        return surfaceDistance;
+    }
+
+
+
     #region Fallback Movement
     private float gravity = -10f;
     private void FallbackMovement()
     {
+#if UNITY_EDITOR
+        Debug.LogWarning("USING FALLBACK MOVEMENT");
+#endif
         transform.localPosition += (inputDirection + (Vector3.up * gravity)) * speed * Time.deltaTime;
 
         Quaternion toGroundRotation = Quaternion.FromToRotation(transform.up, Vector3.up) * transform.rotation;
